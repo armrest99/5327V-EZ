ac855e25e6160a663948bee01b6b26b63fa0b3e8
armrest99 <amrithkrishnakumar99@gmail.com>
2022-03-25 17:49:36 -0700

    added drive forward auton

--- src/autons.cpp
+++ src/autons.cpp
@@ -77,13 +77,13 @@ void drive_example() {
   // for slew, only enable it when the drive distance is greater then the slew distance + a few inches
 
 
-  chassis.set_drive_pid(24, DRIVE_SPEED, true);
+  chassis.set_drive_pid(6, DRIVE_SPEED, true);
   chassis.wait_drive();
 
-  chassis.set_drive_pid(-12, DRIVE_SPEED);
+  chassis.set_drive_pid(-3, DRIVE_SPEED);
   chassis.wait_drive();
 
-  chassis.set_drive_pid(-12, DRIVE_SPEED);
+  chassis.set_drive_pid(-3, DRIVE_SPEED);
   chassis.wait_drive();
 }
 
