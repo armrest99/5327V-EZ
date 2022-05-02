f520b1bd8e6044090d9b0794942070e7e1c3f6ce
armrest99 <amrithkrishnakumar99@gmail.com>
2022-05-01 22:44:02 -0700

    idk

--- include/main.h
+++ include/main.h
@@ -48,7 +48,7 @@ pros::Motor erector(19, MOTOR_GEARSET_36, MOTOR_ENCODER_DEGREES);
 pros::ADIDigitalOut boner_clamp('A');
 pros::ADIDigitalOut ass_clamp('B');
 pros::ADIDigitalOut flipping('C');
-pros::ADIDigitalOut Extendo('D');
+pros::ADIDigitalOut Extendo('D'); 
 pros::ADIDigitalOut pto('E');
 
 // More includes here...
